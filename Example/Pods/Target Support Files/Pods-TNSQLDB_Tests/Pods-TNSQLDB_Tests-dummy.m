af83fe9c72759272d4550450d6d24ef0568a0d29
luhongdeng <luhongdeng@syswin.com>
2018-03-31 23:13:19 +0800

    remove all source

--- Example/Pods/Target Support Files/Pods-TNSQLDB_Tests/Pods-TNSQLDB_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TNSQLDB_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_TNSQLDB_Tests
-@end
--- Example/Pods/Target Support Files/TNSQLDB/TNSQLDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TNSQLDB : NSObject
-@end
-@implementation PodsDummy_TNSQLDB
-@end
